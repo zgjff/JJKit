35ce8d6e4b58e417d4a7af5be11f702663971c95
zgj <zguijie1005@qq.com>
2018-05-08 16:58:48 +0800

    增加placeholder textView

---
+++ Sources/Base/PlaceholderTextView.swift
@@ -0,0 +1,21 @@
+//
+//  PlaceholderTextView.swift
+//  Demo
+//
+//  Created by 郑桂杰 on 2018/5/8.
+//  Copyright © 2018年 郑桂杰. All rights reserved.
+//
+
+import UIKit
+
+class PlaceholderTextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
